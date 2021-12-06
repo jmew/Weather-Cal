c2884802a211182f1294bbbc3f9d1126b369cd6d
Jeffrey Mew <jeffrey.mew@microsoft.com>
2021-12-06 09:40:20 -0800

    Update weather-cal-code.js

--- weather-cal-code.js
+++ weather-cal-code.js
@@ -1314,7 +1314,7 @@ const weatherCal = {
     }
 
     // If it's an error, treat it as a null value.
-    if (typeof weatherDataRaw !== 'undefined' || weatherDataRaw == null || weatherDataRaw.cod) { weatherDataRaw = null }
+    if (typeof weatherDataRaw === 'undefined' || weatherDataRaw == null || weatherDataRaw.cod) { weatherDataRaw = null }
 
     // English continues using the "main" weather description.
     const english = (this.locale.split("_")[0] == "en")
