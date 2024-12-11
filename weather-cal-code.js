5a66ff64d4f84e96b9d7610490733783156fb828
Jeffrey Mew <15910920+jmew@users.noreply.github.com>
2024-12-11 14:48:47 -0800

    Update weather-cal-code.js

--- weather-cal-code.js
+++ weather-cal-code.js
@@ -1183,6 +1183,7 @@ const weatherCal = {
         const weatherReq = "https://api.openweathermap.org/data/2.5/weather?lat=" + this.data.location.latitude + "&lon=" + this.data.location.longitude + "&exclude=minutely,alerts&units=" + this.settings.widget.units + lang + "&appid=" + apiKey
         weatherDataRaw = await new Request(weatherReq).loadJSON()
         this.fm.writeString(cachePath, JSON.stringify(weatherDataRaw))
+        throw new Error(weatherDataRaw)
       } catch (e) {console.error(JSON.stringify(weatherDataRaw))}
     }
 
