e44d7d0f5a8e1f8c682f397701e5a702c259388d
Jeffrey Mew <15910920+jmew@users.noreply.github.com>
2024-12-13 15:18:18 -0800

    Fixed bracket

--- weather-cal-code.js
+++ weather-cal-code.js
@@ -1559,8 +1559,6 @@ const weatherCal = {
     // Show the current condition symbol.
     let mainConditionStack = this.align(currentWeatherStack)
     let mainCondition = mainConditionStack.addImage(this.provideConditionSymbol(weatherData.currentCondition,this.isNight(this.now)))
-    await this.generateAlert(weatherData.currentCondition),[])
-    //TODO
     mainCondition.imageSize = new Size(22,22)
     this.tintIcon(mainCondition, this.format.largeTemp)
     mainConditionStack.setPadding(weatherSettings.showLocation ? 0 : this.padding, this.padding, 0, this.padding)
