50956740aff83c9995d7c81eb9c8279f3794797b
Jeffrey Mew <thehonourablejeffrey@gmail.com>
2024-12-16 14:34:12 -0800

    debug statement

--- weather-cal-code.js
+++ weather-cal-code.js
@@ -2058,31 +2058,7 @@ const weatherCal = {
 
   // Provide a symbol based on the condition.
   provideConditionSymbol(cond,night) {
-
-    // const iconMap = {
-    //   "01d": "bi-brightness-high",     // clear sky (day)
-    //   "01n": "bi-moon-stars",          // clear sky (night)
-    //   "02d": "bi-cloud-sun",           // few clouds (day)
-    //   "02n": "bi-cloud-moon",          // few clouds (night)
-    //   "03d": "bi-cloud",               // scattered clouds (day)
-    //   "03n": "bi-cloud",               // scattered clouds (night)
-    //   "04d": "bi-clouds",              // broken clouds (day)
-    //   "04n": "bi-clouds",              // broken clouds (night)
-    //   "09d": "bi-cloud-drizzle",       // shower rain (day)
-    //   "09n": "bi-cloud-drizzle",       // shower rain (night)
-    //   "10d": "bi-cloud-rain",          // rain (day)
-    //   "10n": "bi-cloud-rain",          // rain (night)
-    //   "11d": "bi-cloud-lightning-rain",// thunderstorm (day)
-    //   "11n": "bi-cloud-lightning-rain",// thunderstorm (night)
-    //   "13d": "bi-cloud-snow",          // snow (day)
-    //   "13n": "bi-cloud-snow",          // snow (night)
-    //   "50d": "bi-cloud-fog",           // mist (day)
-    //   "50n": "bi-cloud-fog"            // mist (night)
-    // };
-    
-    // // Return the mapped icon if it exists; otherwise, fall back to a default icon.
-    // return iconMap[iconCode] || "bi-exclamationmark.circle";
-  
+    this.generateAlert("provide symbol fn: " + night.toString(),[])
     // Define our symbol equivalencies.
     let symbols = {
 
@@ -2107,7 +2083,7 @@ const weatherCal = {
         if (cond == 701 || cond == 741) { return "cloud.fog.fill" }
         return night ? "cloud.fog.fill" : "sun.haze.fill"
       },
-
+      
       // Clear and clouds
       "8": function() {
         if (cond == 800 || cond == 801) { return night ? "moon.stars.fill" : "sun.max.fill" }
